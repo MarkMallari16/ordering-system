c57e3dcded76ef12a725918102647b65cde432a7
Mark Christian Mallari <104248911+MarkMallari16@users.noreply.github.com>
2024-12-05 15:53:59 +0800

    Add files via upload

---
+++ Database.java
@@ -0,0 +1,15 @@
+package mallari_markchristian_g_2;
+
+import java.util.HashMap;
+
+public class Database {
+
+    public static HashMap<Integer, String[]> userDb = new HashMap<Integer, String[]>();
+    public static HashMap<Integer,String[]> productDb = new HashMap<Integer,String[]>();
+    
+    public static void addAdmin() {
+        int adminId = 1000;
+        String[] adminRegInfos = {"", "", "admin", "admin123", "admin"};
+        userDb.put(adminId, adminRegInfos);
+    }
+}
---
+++ Mallari_MarkChristian_G_2.java
@@ -0,0 +1,13 @@
+
+package mallari_markchristian_g_2;
+
+
+public class Mallari_MarkChristian_G_2 {
+
+  
+    public static void main(String[] args) {
+       Login log = new Login();
+       log.show();
+    }
+    
+}
